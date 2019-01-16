9ab87e23a9de0035361111750b03943d66d4f12c
Salvador <salbecerra1023@gmail.com>
2019-01-15 17:12:57 -0800

    bug fixing

--- server.js
+++ server.js
@@ -35,8 +35,8 @@ app.use("/api/users", users);
 if(process.env.NODE_ENV === 'production') {
 	app.use(express.static('client/build'));
 
-	app.get('*', (req, response) => {
-		response.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 	})
 }
 
